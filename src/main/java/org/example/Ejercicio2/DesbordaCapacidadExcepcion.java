591e4d9f429ea80238ed9d365f2c7d617f3c44dd
davidtinamart <dmarttin@myuax.com>
2023-03-21 22:54:26 +0100

    Clase Excepcion ️️

---
+++ src/main/java/org/example/Ejercicio2/DesbordaCapacidadExcepcion.java
@@ -0,0 +1,8 @@
+package org.example.Ejercicio2;
+
+public class DesbordaCapacidadExcepcion extends Exception {
+
+    public DesbordaCapacidadExcepcion() {
+        super("El resultado desborda la capacidad de esta calculadora");
+    }
+}
