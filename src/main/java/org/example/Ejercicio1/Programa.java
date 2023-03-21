da02d07ae2a326a262ed1296b624678ee99f3d75
davidtinamart <dmarttin@myuax.com>
2023-03-21 22:47:57 +0100

    Ejercicio 1 más limpio.

--- src/main/java/org/example/Ejercicio1/Programa.java
+++ src/main/java/org/example/Ejercicio1/Programa.java
@@ -1,25 +1,10 @@
 package org.example.Ejercicio1;
-import java.util.Scanner;
 
-public class Programa {
+public class Programa   {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Introducir un valor: ");
-        double num = teclado.nextDouble();
-        teclado.close();
-        try {
-            double raiz = raizCuadrada(num);
-            System.out.printf("√%.2f = %.2f", num, raiz);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    public static double raizCuadrada(double num) throws Exception {
-        if (num < 0) {
-            throw new Exception("La raíz cuadrada no está definida para un número negativo.");
-        }
-        return Math.sqrt(num);
+        RaizCuadrada r1 = new RaizCuadrada();
+        r1.CalculadoraRaizCuadrada();
     }
 }
